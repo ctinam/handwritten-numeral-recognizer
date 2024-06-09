8f7b457e6991a740b331f15e10b9504d91b5f2f9
Christina Ma <70864545+ctinam@users.noreply.github.com>
2024-06-09 11:29:56 -0400

    Update knn.h

--- src/knn.h
+++ src/knn.h
@@ -1,12 +1,5 @@
 #pragma once
 
-/**
- * You will not be submitting this file, so do not change anything here
- * as it will not be reflected when the automarker is run. If you need any
- * extra structs / definitions, you can add them at the top of your `knn.c`
- * file, so they do not interfere with anything else.
- */
-
 #include <math.h>    // Need this for sqrt()
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,13 +22,10 @@ typedef struct {
     unsigned char *labels;  // List of `num_items` labels [0-9]
 } Dataset;
 
-// These are all the same as A1.
 double distance(Image *a, Image *b);
 int knn_predict(Dataset *data, Image *img, int K);
 
-// This is the same as A2.
 Dataset *load_dataset(const char *filename);
 void free_dataset(Dataset *data);
 
-// New for A3!
 void child_handler(Dataset *training, Dataset *testing, int K, int p_in, int p_out);
